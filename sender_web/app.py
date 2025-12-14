79dc8669c97748e4cb7358c6863c2628ea3decee
Kalp Thakkar <kalpthakkar2001@gmail.com>
2025-12-14 10:28:08 -0600

    Update sender_web app logic

--- sender_web/app.py
+++ sender_web/app.py
@@ -4,9 +4,13 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 import sys
 import uvicorn
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent # sender_web/
+TEMPLATES_DIR = BASE_DIR / "templates"
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
 
 RECEIVER_URL = sys.argv[1] if len(sys.argv) > 1 else "http://localhost:8000"
 
@@ -15,4 +19,4 @@ async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "receiver_url": RECEIVER_URL})
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=5000, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=5000, reload=False)
